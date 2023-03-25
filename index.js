0f495ae8a6fe436230e23656b5e5bc95178ecd94
indexofmayank <mayankdevtiwari>
2023-03-25 20:25:59 +0530

    'forheroku'

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const app = express();
+const cors = require("cors");       
+const port = process.env.PORT || 3000;
 
 
+app.use(cors());
 
 app.get(`/`, (req, res) => {
     res.json("Hii from api");
 });
 
-app.listen(5000, (() => {
+app.listen(port, (() => {
     console.log("server is wokring");
 }))
