83d4c3097199d1dd93d088165c412b1f9d223066
indexofmayank <mayankdevtiwari>
2023-03-25 20:34:47 +0530

    'forRailway'

--- index.js
+++ index.js
@@ -10,6 +10,10 @@ app.get(`/`, (req, res) => {
     res.json("Hii from api");
 });
 
+app.get(`/api/v1`, (req, res) => {
+    res.json("Hii from /api/v1");
+})
+
 app.listen(port, (() => {
     console.log("server is wokring");
-}))
+}));
